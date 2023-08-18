147ad0da855cd8eb047d0724760fa93f1a27e378
Kai Loo <lookai0303@gmail.com>
2023-08-18 14:57:37 +0800

    update, delete

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -40,7 +40,37 @@ namespace CompleteDeveloperNetworkBackend.Controllers
             return Ok(await _context.Users.ToListAsync());
         }
 
-        
-        // Implement Update and Delete methods similarly
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUser(User user)
+        {
+            var userToUpdate = await _context.Users.FindAsync(user.Id);
+            if (userToUpdate == null)
+            {
+                return BadRequest("User not found.");
+            }
+            userToUpdate.Username = user.Username;
+            userToUpdate.Email = user.Email;
+            userToUpdate.PhoneNumber = user.PhoneNumber;
+            userToUpdate.Skillsets = user.Skillsets;
+            userToUpdate.Hobby = user.Hobby;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(int id)
+        {
+            var userToDelete = await _context.Users.FindAsync(id);
+            if (userToDelete == null)
+            {
+                return BadRequest("User not found.");
+            }
+            _context.Users.Remove(userToDelete);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
     }
 }
